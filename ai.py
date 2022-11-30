e75d8f5c24a222deb74ac23c1412b616f3188139
bdavis83 <bdavis83@gmail.com>
2022-11-30 11:33:51 -0500

    Gesture input fucntion added to Player Class

--- ai.py
+++ ai.py
@@ -1,5 +1,4 @@
 class Ai:
 
     def __init__ (self, name):
-        self.name = name
-        self.gesture_list = []
+       pass
--- player.py
+++ player.py
@@ -1,4 +1,14 @@
 class Player:
 
     def __init__(self) -> None:
+       
         pass
+
+    def gesture_input ():
+        gesture_list = ["rock", "paper", "Scissor", "lizard", "spock"]
+        user_input = input ("Enter 0 for rock, 1 for scissor, 2 for paper, 3 for lizard, 4 for spock")
+        return user_input
+
+
+    user_action = gesture_input ()
+    print (user_action)
