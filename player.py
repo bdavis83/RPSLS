5f2317fd9737be7a188453539c272e6380af2e2f
bdavis83 <bdavis83@gmail.com>
2022-12-01 15:43:36 -0500

    fxed get input statement to allow for resubmission if the user enters an invalid selection

--- player.py
+++ player.py
@@ -7,19 +7,23 @@ class Player:
         self.wins = 0
         pass
     
-    def get_input(self):
-        user_input = input (f'Enter 0 to select Rock, 1 to select Paper 2 to select Scissors, 3 for Lizard and 4 for Spock ')
-        try:
-            converted_input = int(user_input)
-        except:
-            print("You entered a value that's not a number")
-        self.chosen_gesture = self.gestures[converted_input]
-        # if user_input != "Rock" or "Paper" or "Scissor":
-        #     input ("User input not valid.  Please re enter an action from the {gesture_list}")
-        # print (f"Player one chooses {user_input}")
+    #def get_input(self):
+    #    user_input = input (f'Enter 0 to select Rock, 1 to select Paper 2 to select Scissors, 3 for Lizard and 4 for Spock ')
+     #   try:
+     #       converted_input = int(user_input)
+     #   except:
+     #       print("You entered a value that's not a number")
+     #   self.chosen_gesture = self.gestures[converted_input]
+        
         
 
-        
+    def get_input(self):
+        user_input = input (f'Enter 0 to select Rock, 1 to select Paper 2 to select Scissors, 3 for Lizard and 4 for Spock ')
+        converted_input = int(user_input)
+        if converted_input < 0 or converted_input > 4:
+            input ("You entered a value that's not a valid selection. Please enter a a valid selection")
+        else:
+            self.chosen_gesture = self.gestures[converted_input]
 
 
    
